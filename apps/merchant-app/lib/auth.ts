7fdf3bf8ff37a46257c8369f3cd66788d699283e
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-07-06 19:32:31 +0530

    Update merchant/lib/ auth.ts

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -1,16 +1,16 @@
 import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions, User, Account } from "next-auth";
 import db from "@repo/db/client";
+import { NextAuthOptions, User, Account } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
+    })
   ],
   callbacks: {
-    async signIn({ user, account }: { user: User; account: Account }) {
+    async signIn({ user, account }: { user: User; account: Account | null }) {
       console.log("hi signin");
 
       if (!user || !user.email) {
@@ -19,24 +19,24 @@ export const authOptions: NextAuthOptions = {
 
       await db.merchant.upsert({
         select: {
-          id: true,
+          id: true
         },
         where: {
-          email: user.email,
+          email: user.email
         },
         create: {
           email: user.email,
           name: user.name,
-          auth_type: account.provider === "google" ? "Google" : "Github",
+          auth_type: account?.provider === "google" ? "Google" : "Github" // Use a prisma type here
         },
         update: {
           name: user.name,
-          auth_type: account.provider === "google" ? "Google" : "Github", 
-        },
+          auth_type: account?.provider === "google" ? "Google" : "Github" // Use a prisma type here
+        }
       });
 
       return true;
-    },
+    }
   },
-  secret: process.env.NEXTAUTH_SECRET || "secret",
+  secret: process.env.NEXTAUTH_SECRET || "secret"
 };
