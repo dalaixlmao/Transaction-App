47ffb3d039ee0f13ed68251d5793f3aaf412691a
Anubhav Aaryan <74524090+dalaixlmao@users.noreply.github.com>
2024-08-09 18:12:11 +0530

    Update provider.tsx

--- apps/user-app/provider.tsx
+++ apps/user-app/provider.tsx
@@ -3,7 +3,6 @@
 
 import { RecoilRoot } from "recoil";
 import { SessionProvider } from "next-auth/react";
-//jjk
 export const Providers = ({children}: {children: React.ReactNode}) => {
     return <RecoilRoot>
         <SessionProvider>
